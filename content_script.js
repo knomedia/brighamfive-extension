96afce17026913ed13c11f6dda35da1efe408986
jsk <knomedia@gmail.com>
2020-01-14 23:10:10 -0700

    add UI to display speed and time of video

--- content_script.js
+++ content_script.js
@@ -1,3 +1,11 @@
+const CN = "BrighamFive"
+const SPEED = CN + '__speed';
+const TIME = CN + '__time';
+
+let intervalId;
+
+// *****************
+// Utils
 function getEl(cn) {
   return document.querySelector(cn)
 }
@@ -7,19 +15,76 @@ function hide(cn) {
   el.hidden = true
 }
 
+
+// *****************
+// UI
+function createUIEl() {
+  var el = document.createElement('div');
+  var domString = `<div class="${CN}">
+    <div class="${TIME}__Outer">
+      Time
+      <div class="${TIME}"></div>
+    </div>
+    <div class="${TIME}__Outer">
+      Speed
+      <div class="${SPEED}"></div>
+    </div>
+    </div>`
+  el.innerHTML =  domString;
+  document.body.appendChild(el.firstChild);
+  return getUI()
+}
+
+function getUI() {
+  return document.querySelector('.' + CN)
+}
+
+function fmtMSS(s){
+  return(s-(s%=60))/60+(9<s?':':':0')+s
+}
+
+function showTime() {
+  let time = fmtMSS(Math.floor(video.currentTime))
+  let el = document.querySelector('.' + TIME)
+  el.innerHTML = time
+}
+
+function showSpeed(speed) {
+  let el = document.querySelector('.' + SPEED)
+  el.innerHTML = speed
+}
+
+function updateUI() {
+  window.setTimeout(function() {
+    showTime()
+    updateUI()
+  }, 1000)
+}
+
+function showUI() {
+  let el = getUI()
+  el.classList.add(CN + "__show")
+  if (intervalId) {
+    window.clearInterval(intervalId)
+  }
+  intervalId = window.setTimeout(function() {
+    hideUI()
+  }, 2000)
+}
+
+function hideUI() {
+  let el = getUI()
+  el.classList.remove(CN + "__show")
+}
+
+
+// *****************
+// VID CONTROL
 function fullVid(cn) {
   let vid = getEl(cn)
   vid.style.maxWidth = "6000px";
   return vid
 }
-
-let toHide = [
-  ".list-container",
-  ".header",
-  ".extra-space-div",
-  ".info",
-  ".footer"
-]
 
 function fullVid(cn) {
   let vidWrapper = getEl(cn)
@@ -31,9 +96,7 @@ function seek(seconds) {
   return video.currentTime
 }
 
-
 function handleSpeed(key) {
-  console.log(key);
   const MAP = {
     "ArrowUp": 0.1,
     "ArrowDown": -0.1
@@ -48,15 +111,18 @@ function handleSpeed(key) {
   let speed = SPEEDS[key]
   if (speed) {
     video.playbackRate = speed
+    showSpeed(video.playbackRate)
   }
   if (delta) {
     let rate = Math.round((video.playbackRate + delta) * 10) / 10
     rate = Math.max(rate, 0.1)
     video.playbackRate = rate
+    showSpeed(video.playbackRate)
   }
   if (speed || delta) {
     let r = video.playbackRate
-    console.log(r);
+    showSpeed(r)
+    showUI()
     const MAX = 2.3;
     const MIN = 0.4
     const LIMITED_VOLUME = 0.05
@@ -68,7 +134,6 @@ function handleSpeed(key) {
     }
   }
 }
-
 function handleSeek(key) {
   const MAP = {
     'f': 5,
@@ -81,7 +146,7 @@ function handleSeek(key) {
   let seconds = MAP[key]
   if (seconds) {
     let res = seek(seconds)
-    console.log(res)
+    showUI()
   }
 }
 
@@ -97,9 +162,23 @@ function onKeyDown(event) {
   handlePlayBack(event.key)
 }
 
+// RUN IT
+
+let toHide = [
+  ".list-container",
+  ".header",
+  ".extra-space-div",
+  ".info",
+  ".footer"
+]
+
 let video = getEl('video')
 window.addEventListener('keydown', onKeyDown)
 
 const VID_WRAPPER = ".player-wrapper-outer"
 toHide.forEach(hide)
 fullVid(VID_WRAPPER)
+
+let el = createUIEl()
+updateUI()
+showSpeed(video.playbackRate)
